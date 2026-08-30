27b87016d893c47a026470e31cf49ee3b212581d
Mohammad Syuhada <mohammadsyuhada@gmail.com>
2026-08-31 05:33:39 +0800

    feat(minarch): support .7z roms via bundled 7zzs extraction

--- workspace/all/minarch/ma_game.h
+++ workspace/all/minarch/ma_game.h
@@ -4,3 +4,4 @@ void Game_open(char* path);
 void Game_close(void);
 int Game_changeDisc(char* path); // returns 1 if the requested disc is loaded afterward
 int extract_zip(char** extensions);
+int extract_7z(char** extensions);
