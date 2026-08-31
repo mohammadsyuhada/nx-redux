fe18de34b378799a6d63d43a8a1d312f4a82e93e
Mohammad Syuhada <mohammadsyuhada@gmail.com>
2026-09-01 07:35:26 +0800

    fix(desktop): use POSIX-safe stdout+stderr redirect in emu pak launch.sh

--- skeleton/SYSTEM/desktop/paks/Emus/GBA.pak/launch.sh
+++ skeleton/SYSTEM/desktop/paks/Emus/GBA.pak/launch.sh
@@ -8,4 +8,4 @@ EMU_TAG=$(basename "$(dirname "$0")" .pak)
 ROM="$1"
 HOME="$USERDATA_PATH"
 cd "$HOME"
-minarch.elf "$CORES_PATH/${EMU_EXE}_libretro.so" "$ROM" &> "$LOGS_PATH/$EMU_TAG.txt"
+minarch.elf "$CORES_PATH/${EMU_EXE}_libretro.so" "$ROM" > "$LOGS_PATH/$EMU_TAG.txt" 2>&1
